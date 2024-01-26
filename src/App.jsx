8c8d61d260989708371a437a9f6a2d3749b385cf
FLRSscript <flrsscript@users.noreply.github.com>
2024-01-25 18:40:10 -0600

    asda

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
   //POST
   const crear = async (form) => {
     try {
-      const response = await fetch(`${url}`, {
+      const response = await fetch(`${url}/crear`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
